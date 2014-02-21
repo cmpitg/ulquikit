bd68e984f8980e930f4d5804e7fed445f9ef2b0f
Duong H. Nguyen <cmpitg@gmail.com>
2014-02-21 14:39:50 +0700

    Add Ruby script to render Markdown documents using Rouge and Redcarpet

---
+++ src/bootstrap/render-markdown.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/env ruby
+
+#
+# This file is part of Ulquikit project.
+#
+# Copyright (C) 2013-2014 Duong H. Nguyen <cmpitg AT gmailDOTcom>
+#
+# Ulquikit is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# Ulquikit is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Ulquikit.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+require 'singleton'
+
+require 'redcarpet'
+require 'rouge'
+require 'rouge/plugins/redcarpet'
+
+class HTMLWithRouge < Redcarpet::Render::HTML
+  include Rouge::Plugins::Redcarpet
+end
+
+class RendererSingleton
+  include Singleton
+
+  MarkdownExtensions = {
+    :with_toc_data  => true,
+    :prettify       => true,
+  }
+
+  RendererOptions = {
+    :autolink             => true,
+    :space_after_headers  => true,
+    :no_intra_emphasis    => true,
+    :fenced_code_blocks   => true,
+    :tables               => true,
+    :highlight            => true,
+    :footnotes            => true,
+  }
+
+  attr_accessor :html_render, :renderer, :css_list, :js_list
+
+  def initialize
+    @html_render   = HTMLWithRouge.new MarkdownExtensions
+    @renderer      = Redcarpet::Markdown.new(@html_render, RendererOptions)
+    @toc_renderer  = Redcarpet::Markdown.new(Redcarpet::Render::HTML_TOC, RendererOptions)
+  end
+
+  def render_file
+    content = ARGF.read
+    puts content
+    toc     = @toc_renderer.render content
+    puts @renderer.render content
+  end
+end
+
+
+RendererSingleton.instance.render_file
