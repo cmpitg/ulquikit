bd1e4dbb8afa2cfa42f59d9e429ca33e87e146f4
Duong H. Nguyen <cmpitg@gmail.com>
2014-02-23 01:47:40 +0700

    Modularize path utils into a separated file

--- src/bootstrap/generate-resources.rkt
+++ src/bootstrap/generate-resources.rkt
@@ -19,11 +19,8 @@
 
 #lang rackjure
 
-(require srfi/1)
 (require racket/runtime-path)
-
-(module+ test
-  (require rackunit))
+(require "utils-path.rkt")
 
 (define-runtime-path +current-dir+ "./")
 
@@ -32,44 +29,6 @@
 ;;
 (define (this-directory)
   (path->string +current-dir+))
-
-;;
-;; Retrieve only file name or last directory name from a path.
-;;
-(define (get-name-from-path path)
-  (~> path
-    (string-split "/")
-    last))
-
-(module+ test
-  (check-equal? (get-name-from-path "/mnt/")        "mnt")
-  (check-equal? (get-name-from-path "/mnt")         "mnt")
-  (check-equal? (get-name-from-path "/mnt/abc.rkt") "abc.rkt"))
-
-;;
-;; Expand and simplify path and convert it to string
-;;
-(define (expand-path path)
-  (~> (expand-user-path path)
-    simplify-path
-    path->string))
-
-;;
-;; Copy files or directories, overwrite older versions.
-;;
-(define (copy-and-overwrite . paths)
-  (let* ([destination (last paths)]
-         [sources     (drop-right paths 1)])
-    (for ([source sources])
-      (let* ([name-only        (get-name-from-path source)]
-             [full-destination (expand-path (string-append destination name-only))]
-             [source           (expand-path source)])
-        (when (or (file-exists? full-destination)
-                  (directory-exists? full-destination))
-          (displayln (~a "-> Deleting: " full-destination))
-          (delete-directory/files full-destination))
-        (displayln (~a "-> Copying " source " to " full-destination))
-        (copy-directory/files source full-destination)))))
 
 (define (main)
   (make-directory* (string-append (this-directory) "../generated-docs/"))
