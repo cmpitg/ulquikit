f9642f4eece33f2264fef955ce78f138a2acb98a
Duong H. Nguyen <cmpitg@gmail.com>
2014-07-13 01:26:12 +0700

    Refactor

--- src/bootstrap/utils-string.rkt
+++ src/bootstrap/utils-string.rkt
@@ -56,3 +56,226 @@
     (~> str/list/reversed/trimmed
       reverse
       list->string)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; process-string
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; All functions in this section must be called within the effect of
+;; process-string.  Otherwise, unexpected behaviors will occur.
+
+(define _string_   (make-parameter ""))
+(define _position_ (make-parameter 0))
+
+(define-syntax-rule (process-string str body ...)
+  (parameterize ([_string_    str]
+                 [_position_  0])
+    body ...))
+
+(define (get-string)
+  (_string_))
+
+(define (get-position)
+  (_position_))
+
+(define get-string-length #λ(string-length (get-string)))
+
+(define (set-position new-position)
+  (_position_ new-position))
+
+(module+ test
+  (process-string "hello world"
+    (check-equal? (get-string)   "hello world")
+    (check-equal? (get-position) 0)
+
+    (set-position                10)
+    (check-equal? (get-position) 10)))
+
+(define (look-for str)
+  (let ([pattern (regexp-quote str)])
+    (regexp-match? pattern (get-string) (inc (get-position)))))
+
+(module+ test
+  (process-string "hello world\n[source\ncode[source"
+    (check-equal? (look-for "[source") #t)
+
+    (set-position 15)
+    (check-equal? (look-for "[source") #t)
+
+    (set-position 29)
+    (check-equal? (look-for "[source") #f))
+
+  (process-string "hello world\n[source\ncode[source"
+    (check-equal? (look-for "h") #f)))
+
+(define (goto-next str)
+  (let ([pattern (regexp-quote str)])
+    (set-position (if (look-for str)
+                      (~> (regexp-match-positions pattern
+                                                  (get-string)
+                                                  (inc (get-position)))
+                        first
+                        car)
+                      (dec (get-string-length))))))
+
+(module+ test
+  (process-string "hello world\n[source\ncode[source"
+    (goto-next "h")
+    (check-equal? (get-position) 30))
+
+  (process-string "hello world\n[source\ncode[source"
+    (goto-next "e")
+    (check-equal? (get-position) 1)
+
+    (goto-next "world")
+    (check-equal? (get-position) 6)))
+
+(define (next-line)
+  (goto-next "\n")
+  (when (< (inc (get-position)) (get-string-length))
+    (set-position (inc (get-position)))))
+
+(module+ test
+  (process-string "hello world\n[source\ncode[source"
+    (next-line)
+    (check-equal? (get-position) 12)
+    (next-line)
+    (check-equal? (get-position) 20)
+    (next-line)
+    (check-equal? (get-position) (dec (get-string-length)))))
+
+(define (goto-backward str)
+  (let* ([length (get-string-length)]
+         [current-position (get-position)])
+    (set-position (- length (process-string (~> (get-string)
+                                              string->list
+                                              reverse
+                                              list->string)
+                              (set-position (- length current-position 1))
+                              (goto-next str)
+                              (get-position))
+                     1))))
+
+(module+ test
+  (process-string "ab"
+    (set-position 1)
+    (goto-backward "a")
+    (check-equal? (get-position) 0))
+  
+  (process-string "hello world\n[source\ncode[source"
+    ;; End of string
+    (set-position (dec (get-string-length)))
+
+    (goto-backward "c")
+    (check-equal? (get-position) 29)
+
+    (goto-backward "[")
+    (check-equal? (get-position) 24)
+
+    (goto-backward "[")
+    (check-equal? (get-position) 12)
+
+    (goto-backward "-")
+    (check-equal? (get-position) 0)))
+
+(define (prev-line)
+  (goto-backward "\n")
+  (goto-backward "\n")
+  (when (equal? #\newline (string-ref (get-string) (get-position)))
+    (set-position (inc (get-position)))))
+
+(module+ test
+  (process-string "hello world\n[source\ncode[source\na"
+    (prev-line)
+    (check-equal? (get-position) 0)
+
+    (set-position (dec (get-string-length)))
+
+    (prev-line)
+    (check-equal? (get-position) 20)
+
+    (prev-line)
+    (check-equal? (get-position) 12)
+
+    (prev-line)
+    (check-equal? (get-position) 0)))
+
+(define (to-beginning-of-line)
+  (goto-backward "\n")
+  (when (equal? (~> (get-string)
+                  (string-ref (get-position)))
+                #\newline)
+    (set-position (inc (get-position)))))
+
+(module+ test
+  (process-string "hello world\n[source\ncode[source"
+    (to-beginning-of-line)
+    (check-equal? (get-position) 0)
+
+    (next-line)
+    (set-position (+ (get-position) 3))
+    (to-beginning-of-line)
+    (check-equal? (get-position) 12)
+
+    (next-line)
+    (set-position (+ (get-position) 3))
+    (to-beginning-of-line)
+    (check-equal? (get-position) 20)))
+
+(define (to-end-of-line)
+  (goto-next "\n")
+  (when (equal? (~> (get-string)
+                  (string-ref (get-position)))
+                #\newline)
+    (set-position (- (get-position) 1))))
+
+(module+ test
+  (process-string "hello world\n[source\ncode[source"
+    (to-end-of-line)
+    (check-equal? (get-position) 10)
+
+    (next-line)
+    (to-end-of-line)
+    (check-equal? (get-position) 18)
+
+    (next-line)
+    (to-end-of-line)
+    (check-equal? (get-position) 30)
+
+    (next-line)
+    (to-end-of-line)
+    (check-equal? (get-position) 30)))
+
+(define (get-line)
+  (let* ([current-position (get-position)]
+         [line-start       (begin
+                             (to-beginning-of-line)
+                             (get-position))]
+         [line-end         (begin
+                             (to-end-of-line)
+                             (get-position))]
+         [line             (substring (get-string)
+                                      line-start
+                                      (inc line-end))])
+    (set-position current-position)
+    line))
+
+(module+ test
+  (process-string "hello world\n[source\ncode[source"
+    (check-equal? (get-line) "hello world")
+
+    (next-line)
+    (check-equal? (get-line) "[source")
+
+    (next-line)
+    (check-equal? (get-line) "code[source")
+
+    (next-line)
+    (check-equal? (get-line) "code[source")))
+
+(define (get-substring #:from from #:to to)
+  (substring (get-string) from to))
+
+(module+ test
+  (check-equal? (trim "     ") "")
+  (check-equal? (trim "  -  ") "-"))
