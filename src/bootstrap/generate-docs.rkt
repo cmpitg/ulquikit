698e83eef2404667a8d8113c27803a92f416f992
Duong H. Nguyen <cmpitg@gmail.com>
2014-07-19 19:06:51 +0700

    Implement get-output-file

--- src/bootstrap/generate-docs.rkt
+++ src/bootstrap/generate-docs.rkt
@@ -21,17 +21,31 @@
 
 #lang rackjure
 
+(require racket/path)
+
 (require "utils-path.rkt")
 
 (require racket/runtime-path)
 (define-runtime-path +this-directory+ ".")
 
-(module+ main
-  (let* ([project-dir        (get-relative-path +this-directory+ "../../")]
-         [src-dir            (get-relative-path project-dir "./src")]
-         [generated-docs-dir (get-relative-path project-dir "./generated-docs/")])
-    (parameterize ([current-directory project-dir])
-      (for ([input-file (get-all-adocs src-dir)])
-        (render-asciidoc input-file
-                         (get-relative-path generated-docs-dir
-                                            (get-output-file input-file)))))))
+(module+ test
+  (require rackunit))
+
+(define (get-output-file input-file)
+  (~> (file-name-from-path input-file)
+    path->string
+    (string-replace ".adoc" ".html")))
+
+(module+ test
+  (check-equal? (get-output-file "/tmp/tmp.adoc") "tmp.html")
+  (check-equal? (get-output-file "/tmp/d.adoc")   "d.html"))
+
+;; (module+ main
+;;   (let* ([project-dir        (get-relative-path +this-directory+ "../../")]
+;;          [src-dir            (get-relative-path project-dir "./src")]
+;;          [generated-docs-dir (get-relative-path project-dir "./generated-docs/")])
+;;     (parameterize ([current-directory project-dir])
+;;       (for ([input-file (get-all-adocs src-dir)])
+;;         (render-asciidoc input-file
+;;                          (get-relative-path generated-docs-dir
+;;                                             (get-output-file input-file)))))))
